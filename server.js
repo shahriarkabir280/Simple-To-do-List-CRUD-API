87ab0aecdb69142eeb96b3655afca9024b036141
Md. Shahriar Kabir <shahriarkabir280@gmail.com>
2026-08-18 12:37:14 +0600

    Stage 1: root and health endpoints

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get("/", (req,res ) =>{
+  res.json({
+    name: "Task API",
+    version: "1.0",
+    endpoints: ["/tasks"]
+  })
+})
+
+app.get("/health", (req, res) =>{
+    res.json({
+      status: "OK"
+    })
 });
 
 app.listen(PORT, () => {
